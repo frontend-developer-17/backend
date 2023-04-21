a79b9fdb705cfcc2052ec1b156b29945c49913dd
Kirill <bojkovk411@gmail.com>
2023-04-21 21:27:20 +0300

    project

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,21 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from "./modules/app/app.module";
-import {ConfigService} from "@nestjs/config";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import {ValidationPipe} from "@nestjs/common";
+import { AppModule } from './modules/app/app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['http://localhost:3000']
-    }
-  });
+  const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port');
-  app.useGlobalPipes(new ValidationPipe())
 
   const config = new DocumentBuilder()
-    .setTitle("Lesson api")
-    .setDescription("This api for lesson")
-    .setVersion("1.0")
+    .setTitle('Lesson api')
+    .setDescription('This api for lesson')
+    .setVersion('1.0')
     .addTag('API')
-    .build()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document)
+  SwaggerModule.setup('api', app, document);
   await app.listen(port);
 }
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,26 +1,22 @@
-import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UsersModule } from "../users/users.module";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import {SequelizeModule} from "@nestjs/sequelize";
-import configurations from "../../configurations";
-import { User } from "../users/models/user.model";
-import { AuthModule } from "../auth/auth.module";
-import { TokenModule } from "../token/token.module";
-import { WatchlistModule } from "../watchlist/watchlist.module";
-import { Watchlist } from "../watchlist/models/watchlist.model";
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import configurations from '../../configurations';
+import { UserModule } from './users/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    isGlobal: true,
-    load: [configurations]
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configurations],
+    }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        dialect: "postgres",
+        dialect: 'postgres',
         host: configService.get('db_host'),
         port: configService.get('db_port'),
         username: configService.get('db_user'),
@@ -28,16 +24,12 @@ import { Watchlist } from "../watchlist/models/watchlist.model";
         database: configService.get('db_name'),
         synchronize: true,
         autoLoadModels: true,
-        models: [User, Watchlist]
-      })
+        models: [],
+      }),
     }),
-    UsersModule,
-    AuthModule,
-    TokenModule,
-    WatchlistModule
+    UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
